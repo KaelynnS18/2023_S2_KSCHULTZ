ec0ac26962af37a949e33157f7df090af804bf31
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-02 15:02:02 -0800

    exercises 1 and 2 from Intro to Java

---
+++ bookexercises/chapter1/Exercises1And2.java
@@ -0,0 +1,11 @@
+package bookexercises.chapter1;
+
+public class Exercises1And2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
