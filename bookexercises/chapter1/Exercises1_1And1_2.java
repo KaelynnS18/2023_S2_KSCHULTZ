be72faeb3eae3188c160e9e5922dbbb6e7fba3d5
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-03 14:47:14 -0800

    finished 2.2 area of circle exercise

--- bookexercises/chapter1/Exercises1_1And1_2.java
+++ bookexercises/chapter1/Exercises1_1And1_2.java
@@ -1,6 +1,6 @@
 package bookexercises.chapter1;
 
-public class Exercises1And2 {
+public class Exercises1_1And1_2 {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
         System.out.println("Welcome to Java");
