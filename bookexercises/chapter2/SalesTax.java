c3ffa40fec4828aeca5f50cb8a99185cd01f0821
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-09 14:30:02 -0800

    finished 2.8

---
+++ bookexercises/chapter2/SalesTax.java
@@ -0,0 +1,11 @@
+package bookexercises.chapter2;
+import java.util.Scanner;
+public class SalesTax {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter purchase amount: ");
+        double purchaseAmount = input.nextDouble();
+        double tax = purchaseAmount * 0.17;
+        System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);
+    }
+}
