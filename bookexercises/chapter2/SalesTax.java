0e3cfb3fd590ce8f1e21fd808f888ea52b7cb541
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-09 20:27:41 -0800

    closed input

--- bookexercises/chapter2/SalesTax.java
+++ bookexercises/chapter2/SalesTax.java
@@ -7,5 +7,6 @@ public class SalesTax {
         double purchaseAmount = input.nextDouble();
         double tax = purchaseAmount * 0.17;
         System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);
+        input.close();
     }
 }
