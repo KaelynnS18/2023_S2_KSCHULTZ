f9e494350c4ba7c5f3c3fdebc6bb085291e3de86
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-10 20:40:42 -0800

    changed division values for tests

--- bookexercises/chapter2/ChapterTwoPractice.java
+++ bookexercises/chapter2/ChapterTwoPractice.java
@@ -5,7 +5,7 @@ public class ChapterTwoPractice {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number to get the remainder when divided by 4: ");
         double number = input.nextDouble();
-        double result = (number % 6);
+        double result = (number % 4);
         System.out.println("The remainder of " + number + " is " + result + " when divided by 4");
         input.close();
 
