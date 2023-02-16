2db4e4536eb014fc97198d779ccf33d487a57dc1
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-16 13:17:38 -0800

    finished more ch 3 exercises

---
+++ bookexercises/chapter3/SubtractionQuiz.java
@@ -0,0 +1,23 @@
+package bookexercises.chapter3;
+import java.util.Scanner;
+public class SubtractionQuiz {
+    public static void main(String[] args) {
+        int number1 = (int)(math.random() * 10);
+        int number2 = (int)(math.random() * 10);
+        if (number1 < number2) {
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+        }
+        System.out.print("what is " + number1 + " - " + number2 + "? ");
+        Scanner input = new Scanner(System.in);
+        int answer = input.nextInt();
+        if (number1 - number2 == answer)
+            System.out.println("You are correct!");
+        else {
+            System.out.println("Your answer is wrong.");
+            System.out.println(number1 + " - " + number2 + " should be " + (number1 - number2));
+        }
+        input.close();
+    }
+}
