b5a856e08c6beb888cc4c3ec64a111de611a1b4c
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-02-23 20:30:24 -0800

    finished ch 3

--- bookexercises/chapter3/TestBooleanOperators.java
+++ bookexercises/chapter3/TestBooleanOperators.java
@@ -1,5 +1,20 @@
 package bookexercises.chapter3;
 import java.util.Scanner;
 public class TestBooleanOperators {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        if (number % 2 == 0 && number % 3 == 0){
+            System.out.println(number + " is divisable by 2 and 3.");
+        }
+        if (number % 2 == 0 || number % 3 == 0){
+            System.out.println(number + " is divisable by 2 or 3.");
+        }
+        if (number % 2 == 0 ^ number % 3 == 0){
+            System.out.println(number + " is divisable by 2 or 3, but not both.");
+        }
+        input.close();
+    }
 }
+//This program does not check to see if neither 2 or 3 works, for example, 19 gets no response
