5b3e0adc193c15d6a96401c51c607a539289c826
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-03-13 15:02:02 -0700

    finished 5.7

--- bookexercises/chapter4/LotteryUsingStrings.java
+++ bookexercises/chapter4/LotteryUsingStrings.java
@@ -1,13 +1,13 @@
-package bookexercises.chapter4;
-import java.util.Scanner;
-public class LotteryUsingStrings {
-    public static void main(String[] args){
-        String lottery = "" + (int)(Math.random() * 10) + (int)(Math.random() * 10);
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter your lottery pick (two digits): ");
-        String guess = input.nextLine();
-        char lotteryDigit1 = lottery.charAt(0);
-        char lotteryDigit2 = lottery.charAt(1);
+//package bookexercises.chapter4;
+//import java.util.Scanner;
+//public class LotteryUsingStrings {
+   // public static void main(String[] args){
+       // String lottery = "" + (int)(Math.random() * 10) + (int)(Math.random() * 10);
+       // Scanner input = new Scanner(System.in);
+       // System.out.print("Enter your lottery pick (two digits): ");
+       // String guess = input.nextLine();
+        //char lotteryDigit1 = lottery.charAt(0);
+       // char lotteryDigit2 = lottery.charAt(1);
 
-    }
-}
+   // }
+//}
