355feac15116a2c24b73bb7c73cc8d9d840292e2
Kaelynn Schultz <112717745+KaelynnS18@users.noreply.github.com>
2023-03-13 14:47:51 -0700

    finished some exercises

---
+++ bookexercises/chapter4/test.java
@@ -0,0 +1,10 @@
+package bookexercises.chapter4;
+
+public class test {
+    public static void main(String[] args){
+        int i = 0;
+        for(System.out.println("Hi") ; i < 10 ; i++){
+            System.out.println(i);
+        }
+    }
+}
